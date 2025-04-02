0eef359baa846e5a036ebee1a1e3fe6f2ef45f89
gabrielfpsanches <gabrielfpsanches@gmail.com>
2025-04-01 21:27:26 -0300

    Add files via upload

---
+++ AULA_01/aula_01_01.py
@@ -0,0 +1,12 @@
+# Programa para calcular as 4 operações matemáticas
+nome = "Gabriel Sanches"
+idade = 39
+nota = 8.5
+num_01 = int(input("digite o primeiro valor: "))
+num_02 = int(input("digite o segundo valor: "))
+adicao = num_01 + num_02
+print(adicao)
+print("A soma dos valores é: ",num_01 + num_02)
+print("A subtração dos valores é: ",num_01 - num_02)
+print("A multiplicação dos valores é: ",num_01 * num_02)
+print("A divisão dos valores é: ",num_01 / num_02)
